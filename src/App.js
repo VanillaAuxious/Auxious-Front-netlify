8eeeb9f34fc46d980df96dfa9812267d5ec3a4f6
yb9255 <yoobin9255@gmail.com>
2022-06-09 14:47:00 +0900

    navbar에서 사용할 svg 아이콘 google fonts에서 가져옴, 클릭 시 임새 포커스 색, 임시 navigate까지 포함

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { saveUserInfo } from './store/userSlice';
 import useAxios from './hooks/useAxios';
 import Authorized from './routes/Authorized';
 import Unauthorized from './routes/Unauthorized';
-import BottomSheet from './components/BottomSheet';
+import NavBar from './components/NavBar';
 
 function App() {
   const dispatch = useDispatch();
@@ -23,9 +23,15 @@ function App() {
   }, [userInformation]);
 
   return (
-    <div>
+    <div
+      style={{
+        width: '360px',
+        height: '720px',
+        overflow: 'hidden',
+      }}>
       {userInformation && hasCookie && <Authorized />}
       {!hasCookie && <Unauthorized />}
+      <NavBar />
     </div>
   );
 }
