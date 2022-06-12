3506690aae58da233e9bbcb7a75f3f905818d2ff
yb9255 <yoobin9255@gmail.com>
2022-06-12 21:21:11 +0900

    aws와 연결

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { DEV, PROD } from '../utils/constants';
+import { DEV, PROD, TEST } from '../utils/constants';
 
 async function useAxios(url, method = 'get', body = null, headers = null) {
   let API_URL = null;
@@ -9,7 +9,10 @@ async function useAxios(url, method = 'get', body = null, headers = null) {
     API_URL = process.env.REACT_APP_API_URL_LOCAL;
   }
 
-  if (process.env.REACT_APP_ENV === PROD) {
+  if (
+    process.env.REACT_APP_ENV === PROD ||
+    process.env.REACT_APP_ENV === TEST
+  ) {
     API_URL = process.env.REACT_APP_API_URL_PROD;
   }
 
