dec6adb67a52e647ca15a1a62f651b8840e41657
yb9255 <yoobin9255@gmail.com>
2022-06-13 22:35:41 +0900

    common의 LoadingSpinner 수정

--- src/components/LoadingSpinner.js
+++
@@ -1,7 +0,0 @@
-export default function LoadingSpinner() {
-  return (
-    <div className='loading-spinner-container'>
-      <img src='/img/loading_blue_simple.gif' alt='Loading...' />
-    </div>
-  );
-}
--- src/components/LoginFallback.js
+++ src/components/LoginFallback.js
@@ -3,9 +3,9 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
 import useAxios from '../hooks/useAxios';
+import LoadingSpinner from '../common/LoadingSpinner';
 
 import { saveUserInfo } from '../store/userSlice';
-import LoadingSpinner from './LoadingSpinner';
 
 export default function LoginFallback() {
   const location = useLocation();
