a8a9477f7d69ceff411cf370dbfcdedbefb3904c
hkh9601 <hkh9601@gmail.com>
2022-06-16 02:02:10 +0900

    Pdf백그라운드 및 캔버스안에서 영역제어 완료

--- src/pages/SigningDocument.js
+++ src/pages/SigningDocument.js
@@ -1,12 +1,24 @@
-import { useRef, useLayoutEffect } from 'react';
+import { useRef, useEffect } from 'react';
 
 export default function SigningDocument() {
-  const ref = useRef();
+  const ref = useRef(null);
+  const image = new Image();
 
-  useLayoutEffect(() => {
+  image.height = '750px';
+  image.width = '100%';
+  image.src =
+    'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMTAzMTdfOTgg%2FMDAxNjE1OTcyMzM0MDE0.P1nvKfXOCW_XKpIpVZuJ6RpICT8M2m-4-qbpMPBvhRcg.-27LrnlmDYJCEzucXFXsa3SFAT0Wse5KoxcDGRRI-3Eg.PNG.julian2020%2F%25B4%25EB%25B8%25AE%25C0%25CE_%25C0%25A7%25C0%25D3%25C0%25E5_%25283%2529.png&type=sc960_832';
+
+  useEffect(() => {
+    if (!ref) return;
+
     const canvas = ref.current;
     const ctx = canvas.getContext('2d');
     const color = '#000000';
+    ctx.drawImage(image, 0, 0, 350, 750);
+    ctx.strokeRect(170, 351, 130, 130);
+    const input = document.createElement('input');
+    // input.createTextNode('한경훈');
 
     let touchesArray = [];
 
@@ -15,7 +27,14 @@ export default function SigningDocument() {
       const touches = event.changedTouches;
 
       for (let i = 0; i < touches.length; i++) {
-        touchesArray.push(touches[i]);
+        if (
+          touches[i].clientX < 300 &&
+          170 < touches[i].clientX &&
+          touches[i].clientY < 481 &&
+          351 < touches[i].clientY
+        ) {
+          touchesArray.push(touches[i]);
+        }
 
         ctx.beginPath();
         ctx.fillStyle = color;
@@ -28,17 +47,24 @@ export default function SigningDocument() {
       const touches = event.changedTouches;
 
       for (let i = 0; i < touches.length; i++) {
-        const index = ongoingTouchIndexById(touches[i].identifier);
+        if (
+          touches[i].clientX < 300 &&
+          170 < touches[i].clientX &&
+          touches[i].clientY < 481 &&
+          351 < touches[i].clientY
+        ) {
+          const index = ongoingTouchIndexById(touches[i].identifier);
 
-        if (index >= 0) {
-          ctx.beginPath();
-          ctx.moveTo(touchesArray[index].pageX, touchesArray[index].pageY);
-          ctx.lineTo(touches[i].pageX, touches[i].pageY);
-          ctx.lineWidth = 4;
-          ctx.strokeStyle = color;
-          ctx.stroke();
+          if (index >= 0) {
+            ctx.beginPath();
+            ctx.moveTo(touchesArray[index].pageX, touchesArray[index].pageY);
+            ctx.lineTo(touches[i].pageX, touches[i].pageY);
+            ctx.lineWidth = 4;
+            ctx.strokeStyle = color;
+            ctx.stroke();
 
-          touchesArray.splice(index, 1, touches[i]);
+            touchesArray.splice(index, 1, touches[i]);
+          }
         }
       }
     };
@@ -86,11 +112,11 @@ export default function SigningDocument() {
     canvas.addEventListener('touchend', handleTouchEnd);
     canvas.addEventListener('touchmove', handleTouchMove);
     canvas.addEventListener('touchcancel', handleTouchCancel);
-  }, []);
+  }, [ref]);
 
   return (
     <>
-      <canvas ref={ref} width='300' height='300'></canvas>
+      <canvas ref={ref} width='350px' height='750px'></canvas>
     </>
   );
 }
--- src/routes/Unauthorized.js
+++ src/routes/Unauthorized.js
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 
 import Login from '../pages/Login';
 import LoginFallback from '../components/LoginFallback';
+import SigningDocument from '../pages/SigningDocument';
 
 export default function Unauthorized() {
   return (
@@ -10,6 +11,7 @@ export default function Unauthorized() {
         <Route path='/' element={<Navigate replace to='/login' />} />
         <Route path='/login' element={<Login />}>
           <Route path='github' element={<LoginFallback />} />
+          <Route path='/sign' element={<SigningDocument />} />
         </Route>
       </Routes>
     </>
