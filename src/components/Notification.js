03231de5d934b98ffc25dee14183b47d9f88d61c
rnfltpgus <jpp901220@gmail.com>
2022-06-16 23:40:57 +0900

    메시지 구현 및 메시지 클릭 시, 경매번호 상세페이지 이동 구현

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,14 +1,25 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import toast, { Toaster } from 'react-hot-toast';
 import { onMessageListener } from '../firebase';
 
 function Notification() {
-  const [notification, setNotification] = useState({ title: '', body: '', buildingId: '' });
+  const navigate = useNavigate();
+  const [notification, setNotification] = useState({
+    title: '',
+    body: '',
+    buildingId: '',
+  });
   const notify = () => toast(<ToastDisplay />);
+
+  const handleNotification = () => {
+    toast.remove();
+    navigate(`/detail/${notification?.buildingId}`);
+  };
 
   const ToastDisplay = () => {
     return (
-      <div onClick={() => toast.remove()}>
+      <div onClick={handleNotification}>
         <p>
           <b>{notification?.title}</b>
         </p>
