1838c4561a2516798bf91125e6b993e296fbf4d3
yb9255 <yoobin9255@gmail.com>
2022-06-18 02:55:33 +0900

    바텀시트 경로 수정

--- src/components/BottomSheet.js
+++ src/components/BottomSheet.js
@@ -2,7 +2,7 @@ import useBottomSheet from '../hooks/useBottomSheet';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import './BottomSheet.scss';
+import './BottomSheet.css';
 
 function BottomSheet({ data }) {
   const navigate = useNavigate();
